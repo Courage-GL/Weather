13c2e9a92560c7c68ba344da8e9b6798cc615222
xiaoalei <1450030827@qq.com>
2018-06-15 11:06:21 +0800

    第三次上传

---
+++ app/src/debug/java/com/xiaoalei/android/weather/DebugStethoHelper.java
@@ -0,0 +1,22 @@
+package com.xiaoalei.android.weather;
+
+import android.content.Context;
+
+import com.xiaoalei.android.weather.util.StethoHelper;
+import com.facebook.stetho.Stetho;
+import com.facebook.stetho.okhttp3.StethoInterceptor;
+
+import okhttp3.OkHttpClient;
+
+public class DebugStethoHelper implements StethoHelper {
+
+    @Override
+    public void init(Context context) {
+        Stetho.initializeWithDefaults(context);
+    }
+
+    @Override
+    public OkHttpClient.Builder addNetworkInterceptor(OkHttpClient.Builder builder) {
+        return builder.addNetworkInterceptor(new StethoInterceptor());
+    }
+}
